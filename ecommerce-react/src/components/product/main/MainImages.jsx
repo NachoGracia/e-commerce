eb662acc8d1dd6bc6bd66286ba88ef70d4cb5b92
NachoGracia <gracianacho@gmail.com>
2024-05-23 11:49:43 +0200

    modal added

--- ecommerce-react/src/components/product/main/MainImages.jsx
+++ ecommerce-react/src/components/product/main/MainImages.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import SliderProduct from "./SliderProduct";
 
 const MainImages = ({ imgs, imgsSmall }) => {
-  return <SliderProduct imgs={imgs} imgsSmall={imgsSmall} />;
+  const [modal, setModal] = useState(false);
+
+  const handleOpenModal = () => {
+    window.innerWidth > 767 && setModal(true);
+  };
+  const handleCloseModal = () => {
+    setModal(false);
+  };
+  return (
+    <>
+      <SliderProduct
+        imgs={imgs}
+        imgsSmall={imgsSmall}
+        className="grid md:grid-cols-4 md:gap-4 "
+        handleOpenModal={handleOpenModal}
+      />
+      {modal && (
+        <SliderProduct
+          modal={modal}
+          imgs={imgs}
+          imgsSmall={imgsSmall}
+          className="hidden md:absolute  md:bottom-1/2 md:right-1/2 md:grid  md:max-w-lg md:translate-x-1/2 md:translate-y-1/2 md:grid-cols-4 md:gap-4"
+          handleCloseModal={handleCloseModal}
+        />
+      )}
+    </>
+  );
 };
 
 export default MainImages;
