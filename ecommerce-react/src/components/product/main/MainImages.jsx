0552fc00ac1c481c4f0716e2b71db3c8ffacf57b
NachoGracia <gracianacho@gmail.com>
2024-06-06 18:57:45 +0200

    bg blur added

--- ecommerce-react/src/components/product/main/MainImages.jsx
+++ ecommerce-react/src/components/product/main/MainImages.jsx
@@ -19,13 +19,17 @@ const MainImages = ({ imgs, imgsSmall }) => {
         handleOpenModal={handleOpenModal}
       />
       {modal && (
-        <SliderProduct
-          modal={modal}
-          imgs={imgs}
-          imgsSmall={imgsSmall}
-          className="hidden md:absolute  md:bottom-1/2 md:right-1/2 md:grid  md:max-w-lg md:translate-x-1/2 md:translate-y-1/2 md:grid-cols-4 md:gap-4"
-          handleCloseModal={handleCloseModal}
-        />
+        <>
+          <SliderProduct
+            modal={modal}
+            imgs={imgs}
+            imgsSmall={imgsSmall}
+            className="z-10 hidden md:absolute  md:bottom-1/2 md:right-1/2 md:grid  md:max-w-lg md:translate-x-1/2 md:translate-y-1/2 md:grid-cols-4 md:gap-4"
+            handleCloseModal={handleCloseModal}
+          />
+
+          <span className="fixed left-0 top-0 h-full w-full bg-black/70 backdrop-blur-sm"></span>
+        </>
       )}
     </>
   );
