0b4d63e6375a942cefdf06eed7d808d1c5c9abd6
NachoGracia <gracianacho@gmail.com>
2024-06-17 11:53:31 +0200

    fixed menu

--- ecommerce-react/src/components/header/NavLinkHeader.jsx
+++ ecommerce-react/src/components/header/NavLinkHeader.jsx
@@ -1,8 +1,8 @@
 const NavLinkHeader = ({ name }) => {
   return (
-    <a href="#" className="group relative py-8">
+    <a href="#" className="group relative py-2">
       <span className="group-hover:text-orange-primary">{name}</span>
-      <span className="group-hover:bg-orange-primary absolute bottom-0 block h-[3px] w-full scale-x-0 transition-all duration-300 group-hover:scale-x-100 "></span>
+      <span className="absolute bottom-0 block h-[3px] w-full scale-x-0 transition-all duration-300 group-hover:scale-x-100 group-hover:bg-orange-primary "></span>
     </a>
   );
 };
